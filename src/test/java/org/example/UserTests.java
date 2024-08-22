60b18c283d6add4b8d5fa3d764bf849cc1f15955
Ahmed1208 <ahmedhoss14@yahoo.com>
2024-08-22 12:25:11 +0300

    first commit

---
+++ src/test/java/org/example/UserTests.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTests {
+
+    @Test
+    void testCalcFunction()
+    {
+        User user = new User();
+        assertEquals(15,user.calc(5));
+        assertEquals(10,user.calc(0));
+    }
+
+
+}
