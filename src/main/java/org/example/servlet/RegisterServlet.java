70e803b09e59731936eeb4bfbab2ad0fcc997f93
Mohamed-Ashraf1 <mmidoaashraf@gmail.com>
2024-09-04 22:09:42 +0300

    register form

---
+++ src/main/java/org/example/servlet/RegisterServlet.java
@@ -0,0 +1,53 @@
+package org.example.servlet;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.entity.User;
+import org.example.service.UserService;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String gender = req.getParameter("gender");
+        String dob = req.getParameter("dob");
+        String street = req.getParameter("street");
+        String city = req.getParameter("city");
+        String country = req.getParameter("country");
+        String phone = req.getParameter("phone");
+        EntityManagerFactory emf=(EntityManagerFactory) req.getServletContext().getAttribute("emf");
+        UserService userService=new UserService(emf.createEntityManager());
+        User u=  userService.registerNewUser(name, email, password, street, city, country, phone, dob, gender);
+        if (u!=null){
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.println("<h1>Registration Details</h1>");
+            out.println("<p>Name: " + name + "</p>");
+            out.println("<p>Email: " + email + "</p>");
+            out.println("<p>Password: " + password + "</p>");
+            out.println("<p>Gender: " + gender + "</p>");
+            out.println("<p>DOB: " + dob + "</p>");
+            out.println("<p>Street: " + street + "</p>");
+            out.println("<p>City: " + city + "</p>");
+            out.println("<p>Country: " + country + "</p>");
+            out.println("<p>Phone: " + phone + "</p>");
+
+        }else {
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.println("<h1>Registration Failed</h1>");
+        }
+    }
+
+
+}
